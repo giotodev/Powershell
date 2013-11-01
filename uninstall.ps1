bc4ac51503424c900696df044d886109d54cffd7
Tim <tmmtsmith@gmail.com>
2013-11-01 14:05:21 -0500

    Uninstall multiple apps

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,8 +1,16 @@
-## Product a list of applications
+##  Product a list of applications
 $apps = Get-WmiObject -Query "SELECT * FROM Win32_Product"
 
-## Specific
+##  pecific
 $spec = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%specific%'"
 
-## Uninstall
+##  Uninstall
 $spec.Uninstall()
+
+##  Foreach approach
+$apps = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%ProductOne%' OR Name LIKE '%ProductTwo%'"
+
+foreach ($app in $apps)
+{
+  $app.Uninstall()
+}
