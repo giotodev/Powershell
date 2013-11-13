ea174ab09621be504157ac5514983b04905f0a85
Tim <tmmtsmith@gmail.com>
2013-11-13 05:12:25 -0600

    Update ExportQueryToCSV.ps1

--- ExportQueryToCSV.ps1
+++ ExportQueryToCSV.ps1
@@ -1,11 +1,8 @@
 ��## ONLY change these values inside the parentheses (all are strings so the values need to remain in parenthesis)
 
 $SQLServer = "OurServerName"
-
 $Database = "OurDatabaseName"
-
 $filename = "C:\Users\Public\OurFileName.csv"
-
 $Query = "OurQuery"
 
 
@@ -13,25 +10,13 @@ $Query = "OurQuery"
 ## These values don't change
 
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-
-$SqlConnection.ConnectionString =
-
-"Server=$SQLSERVER;Database=$DATABASE;Integrated Security=True"
-
+$SqlConnection.ConnectionString = "Server=$SQLSERVER;Database=$DATABASE;Integrated Security=True"
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-
 $SqlCmd.CommandText = $Query
-
 $SqlCmd.Connection = $SqlConnection
-
 $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-
 $SqlAdapter.SelectCommand = $SqlCmd
-
 $DataSet = New-Object System.Data.DataSet
-
 $SqlAdapter.Fill($DataSet)
-
 $SqlConnection.Close()
-
 $DataSet.Tables[0] | export-csv -NoTypeInformation $filename
