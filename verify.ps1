af9cb18eb343454546d5346c644bdcb58db6dd4b
Tim <tmmtsmith@gmail.com>
2013-12-07 12:26:27 -0600

    Create verify.ps1

---
+++ verify.ps1
@@ -0,0 +1,20 @@
+clear
+$fs = Get-ChildItem "D:\Backups\" -Filter *.BAK
+$fs = $fs -replace(".BAK","")
+
+foreach ($f in $fs)
+{
+    $full = "D:\Backups\" + $f + ".BAK"
+    $q = "RESTORE VERIFYONLY FROM DISK = '" + $full + "'"
+    "
+    Testing the BAK file " + $f + " ..."
+    
+    try
+    {
+        Invoke-Sqlcmd -Query $q -ServerInstance "SERVER\INSTANCE" -Verbose -EA SilentlyContinue
+    }
+    catch
+    {
+        "I haz teh errorz"
+    }
+}
