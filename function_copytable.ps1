53ac537afa505473aebdcb7e4e2eec473eba249a
Tim <tmmtsmith@gmail.com>
2014-02-03 16:15:46 -0600

    Function that copies tables

---
+++ function_copytable.ps1
@@ -0,0 +1,43 @@
+Function CopyTable($srcServer, $srcDatabase, $destServer, $destDatabase, $bothTable)
+{
+
+    $src = New-Object System.Data.SqlClient.SQLConnection
+    $dest = New-Object System.Data.SqlClient.SQLConnection
+    $src.ConnectionString = "SERVER=" + $srcServer + ";DATABASE=" + $srcDatabase + ";Integrated Security=True"
+    $dest.ConnectionString = "SERVER=" + $destServer + ";DATABASE=" + $destDatabase + ";Integrated Security=True"
+    $data = "SELECT * FROM " + $bothTable
+    $getData = New-Object System.Data.SqlClient.SqlCommand($data, $src)
+
+    try
+    {
+        $src.Open()
+        $dest.Open()
+        [System.Data.SqlClient.SqlDataReader] $reading = $getData.ExecuteReader()
+
+        try
+        {
+            $copy = New-Object Data.SqlClient.SqlBulkCopy($dest)
+            $copy.DestinationTableName = $bothTable
+            $copy.WriteToServer($reading)
+            "Job step successful."
+        }
+        catch [System.Exception]
+        {
+            $ex = $_.Exception
+            throw($ex.Message)
+        }
+        finally
+        {
+            $reading.Close()
+            $src.Close()
+            $copy.Close()
+        }
+    }
+    catch
+    {
+        throw("Cannot open a connection to " + $destServer + ".")
+    }
+}
+
+CopyTable -srcServer "SERVONE\IN" -srcDatabase "DBOne" -destServer "SERVTWO\IN" -destDatabase "DBTwo" -bothTable "Table"
+
