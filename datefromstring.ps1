e8512ed23d13d2a3b1f97ab6f81e9c6dc493f512
Tim <tmmtsmith@gmail.com>
2014-02-24 19:12:36 -0600

    Extract date from first line string

--- datefromstring.ps1
+++ datefromstring.ps1
@@ -13,8 +13,8 @@ Function ReturnDate ($file)
     $f = $c[0]
     $f = $f.Substring(0,20)
     $f = [RegEx]::Matches($f,"(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})") | Select-Object -ExpandProperty Value
-    $sqlDate = $f.Substring(0,4) + "-" + $f.Substring(5,2) + "-" + $f.Substring(7,2)
-    $sqlDateTime = $f.Substring(0,4) + "-" + $f.Substring(5,2) + "-" + $f.Substring(7,2) + " " + $f.Substring(9,2) + ":" + $f.Substring(11,2) + ":" + $f.Substring(13)
+    $sqlDate = $f.Substring(0,4) + "-" + $f.Substring(4,2) + "-" + $f.Substring(6,2)
+    $sqlDateTime = $f.Substring(0,4) + "-" + $f.Substring(4,2) + "-" + $f.Substring(6,2) + " " + $f.Substring(8,2) + ":" + $f.Substring(10,2) + ":" + $f.Substring(12,0)
     $sqlDate, $sqlDateTime
 }
 
