c05c77191059417c3f5effd3fb26fac193eb1d36
Tim <tmmtsmith@gmail.com>
2014-03-03 13:47:16 -0600

    Reverse strings (or numbers)

---
+++ reversestring.ps1
@@ -0,0 +1,17 @@
+## Strings
+
+Function Reverse ($s)
+{
+    $r = $s -split ""
+    [array]::Reverse($r)
+    return $r -join ''
+}
+
+Reverse -s "This is like the REVERSE function in TSQL"
+
+## Numbers
+
+$x = 12363748374
+$x = $x.ToString()
+$x = Reverse -s $x
+$x
