aba510389eb259ace16189799c0ccafc43d5a6f0
Tim <tmmtsmith@gmail.com>
2014-03-26 05:22:47 -0500

    Added verify backups

---
+++ AddGitIgnore.ps1
@@ -0,0 +1,14 @@
+﻿$path = "C:\GitHub\Projects" ## GitHub location
+$gFolders = Get-ChildItem -path $path | Select-Object Name
+
+foreach ($f in $gFolders)
+{
+    $x = $path + "\" + $f.Name + "\.gitignore"
+    if (Test-Path $x)
+    {
+    }
+    else
+    {
+        Write-Host "Git ignore file missing for the" $f.Name "project."
+    }
+}
