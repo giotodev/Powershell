578e96a5de8610cf2fe4d21e68a0368d497bed0c
Tim <tmmtsmith@gmail.com>
2014-04-01 15:05:31 -0500

    Rename files and increment
    Useful for SAMSUNG GALAXY III images (who did the naming convention on
    that phone?)

---
+++ RenameFilesAndIncrement.ps1
@@ -0,0 +1,19 @@
+﻿$loc = "C:\Vacation\"
+$fs = Get-ChildItem $loc -Filter *.png
+$x = 1
+
+Function RenameWithDateAndIncrement($locationPath, $fileName, $extension, $inc)
+{
+    $date = Get-Date -uFormat "%Y%m%d"
+
+    $old = $locationPath + $fileName
+    $new = $locationPath + $inc + "_" + $date + $extension
+
+    Rename-Item $old $new
+}
+
+foreach ($f in $fs)
+{
+    RenameWithDateAndIncrement -locationPath $loc -fileName $f -extension ".png" -inc $x
+    $x = $x + 1
+}
