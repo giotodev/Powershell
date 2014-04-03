21228de5000259165f7ff193f59e5764e235a233
Tim <tmmtsmith@gmail.com>
2014-04-03 04:59:25 -0500

    J Bit
    Weakness: automate needed

---
+++ JBit.ps1
@@ -0,0 +1,20 @@
+﻿$scon = New-Object System.Data.SqlClient.SqlConnection
+$scon.ConnectionString = "SERVER=SRV;Integrated Security=true;DATABASE=DB"
+$add = New-Object System.Data.SqlClient.SqlCommand
+$add.Connection = $scon
+$ev = @()
+$x = Get-Content "C:\Files\file.csv" | ConvertFrom-Csv
+foreach ($v in $x)
+{
+    ##$ev += $v.high.ToString() + "," + $v.timestamp.ToString() #JB
+    $ev += "('" +$v.high.ToString() + "','" + $v.timestamp + "'),"
+}
+
+$ev = "VALUES " + $ev
+$ev = $ev.Substring(0,$ev.Length-1)
+$ev
+
+$scon.Open()
+$add.CommandText = "INSERT INTO AddBit " + $ev
+$add.ExecuteNonQuery()
+$scon.Close()
