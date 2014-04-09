57cf6d31aecbbd248cda56d9a7c53613ab8d88c8
Tim <sqlinsix@gmail.com>
2014-04-09 12:11:29 -0500

    Automatically add programs to startup

---
+++ AddToStartUp.ps1
@@ -0,0 +1,14 @@
+﻿Function AddToStartup($location, $name)
+{
+
+    $u = [Environment]::UserName
+    $startup = "C:\Users\" + $u + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" + $name + ".lnk"
+
+    $short = New-Object -ComObject WScript.Shell
+    $sh = $short.CreateShortcut($startup)
+    $sh.TargetPath = $location
+    $sh.Save()
+
+}
+
+AddToStartup -location "C:\Program Files (x86)\Program\Program.exe" -name "prog"
