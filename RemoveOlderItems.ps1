a39cd6cc5847fba7745f81fc7a17a15dd5187ac4
Tim <sqlinsix@gmail.com>
2014-04-09 15:18:14 -0500

    Remove older items

---
+++ RemoveOlderItems.ps1
@@ -0,0 +1,10 @@
+﻿## If importing a file, and only needing to import the latest file, this will remove all older files and keep the most recent file
+## Put this in a step before the import
+
+Function RemoveEarliestFiles($loc)
+{
+    $fs = Get-ChildItem -Path $loc -Recurse | Where-Object {$_.PsIsContainer -ne $True}
+    $fs | Sort-Object LastWriteTime | Select-Object -First ($files.Count - 1) | Remove-Item
+}
+
+RemoveEarliestFiles -loc "C:\files\"
