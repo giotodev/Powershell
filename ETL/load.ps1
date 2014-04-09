df4c609bd5eb629c70be789426e3645f9d7e7d31
Tim <sqlinsix@gmail.com>
2014-04-09 09:34:19 -0500

    PS Load 1

---
+++ ETL/load.ps1
@@ -0,0 +1,10 @@
+﻿## Set-ExecutionPolicy unrestricted
+## Import-Module SQLPS
+
+Function LoadCSV ($server, $database, $table)
+{
+    $table = "SeveralColumns"
+    invoke-sqlcmd -query "SELECT * FROM $table" -database $database -serverinstance $server | export-csv -path "C:\files\$table.csv"
+}
+
+## Set-ExecutionPolicy restricted
