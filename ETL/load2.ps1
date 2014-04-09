ecc53a00a88dbc80a5c06506ed43449a50386a6b
Tim <sqlinsix@gmail.com>
2014-04-09 11:19:13 -0500

    PS Load 2

---
+++ ETL/load2.ps1
@@ -0,0 +1,19 @@
+﻿Function PSLoad($server, $database, $query, $location)
+{
+    $scon = New-Object System.Data.SqlClient.SqlConnection
+    $scon.ConnectionString = "SERVER=" + $server + ";DATABASE=" + $database + ";Integrated Security=true"
+
+    $cmd = New-Object System.Data.SqlClient.SqlCommand
+    $ad = New-Object System.Data.SqlClient.SqlDataAdapter
+    $ds = New-Object System.Data.DataSet
+    $scon.Open()
+    $cmd.CommandText = $query
+    $cmd.Connection = $scon
+    $ad.SelectCommand = $cmd
+    $ad.Fill($ds)
+    $scon.Close()
+    $ds.Tables[0] | Export-Csv -NoTypeInformation $location
+    
+}
+
+PSLoad -server "SEV" -database "DB" -query "SELECT * FROM SeveralColumns" -location "C:\files\sev.csv"
