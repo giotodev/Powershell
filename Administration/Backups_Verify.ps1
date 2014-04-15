ff649664cdf8b92a540ec181ff2d1ef57797a06f
Tim <tmmtsmith@gmail.com>
2014-04-15 04:22:28 -0500

    Production test

---
+++ Administration/Backups_Verify.ps1
@@ -0,0 +1,28 @@
+﻿Function VerifyBackups($server, $bakLocation)
+{
+    $scon = New-Object System.Data.SqlClient.SqlConnection("SERVER=" + $server + ";DATABASE=master;Integrated Security=true")
+    $scon.FireInfoMessageEventOnUserErrors = $true
+    $cmd = New-Object System.Data.SqlClient.SqlCommand
+    $cmd.Connection = $scon
+    $fs = Get-ChildItem $bakLocation -filter *.BAK
+    
+    foreach ($f in $fs)
+    {
+        $ef = $f.BaseName
+        $full = $bakLocation + $f
+        $cmd = "RESTORE VERIFYONLY FROM DISK = '" + $full + "'"
+
+        try
+        {
+            $scon.Open()
+            $cmd.ExecuteNonQuery()
+            $scon.Close()
+        }
+        catch
+        {
+            Write-Host "Unable to verify."
+        }
+    }
+}
+
+VerifyBackups -server "TIMOTHY\SQLEXPRESS" -bakLocation "D:\Backup\"
