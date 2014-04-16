d0bfd3f66d18837ec264acfaeafa5d5790466550
Tim <sqlinsix@gmail.com>
2014-04-16 14:41:36 -0500

    Unzip multiple zip files

---
+++ UnzipMultipleZipFiles.ps1
@@ -0,0 +1,13 @@
+﻿Function ZipEverything($src, $dest)
+ {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+    $zps = Get-ChildItem $src -Filter *.zip
+
+    foreach ($zp IN $zps)
+    {
+        $all = $src + $zp
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($all, $dest)
+    }
+ }
+
+ ZipEverything -src "\\dfsrvetl\loadstore\MLS-CL\incoming\" -dest "\\dfsrvetl\loadstore\MLS-CL\extract\"
