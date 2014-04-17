3542fd4cf83f16c62110bf3103a65232b5352bac
Tim <tmmtsmith@gmail.com>
2014-04-17 04:44:34 -0500

    Administration zip

---
+++ Administration/UnzipMultipleZipFiles.ps1
@@ -0,0 +1,18 @@
+﻿Function ZipEverything($src, $dest)
+ {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+    $zps = Get-ChildItem $src -Filter *.zip
+
+    foreach ($zp IN $zps)
+    {
+        $all = $src + $zp
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($all, $dest)
+    }
+
+    ## Server, Database, Table
+    $scon = New-Object System.Data.SqlClient.SqlConnection
+    $scon.ConnectionString = "SERVER=" + $server + ";DATABASE=" + $database + ";Integrated Security=true"
+ }
+
+ ZipEverything -src "\\sourcelocation\tracefiles\" -dest "\\destinationlocation\tracefiles\"
+
