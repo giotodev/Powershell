3e2bfb62e68da1bde6526fb554ddc37e4cfd5167
Tim <tmmtsmith@gmail.com>
2014-04-17 10:35:27 -0500

    Added exceptions

--- UnzipMultipleZipFiles.ps1
+++ UnzipMultipleZipFiles.ps1
@@ -1,13 +1,42 @@
-﻿Function ZipEverything($src, $dest)
- {
-    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-    $zps = Get-ChildItem $src -Filter *.zip
+Function ZipEverything($src, $dest)
+{
+   [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+   $zps = Get-ChildItem $src -Filter *.zip
 
-    foreach ($zp IN $zps)
-    {
-        $all = $src + $zp
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($all, $dest)
-    }
- }
+   foreach ($zp IN $zps)
+   {
+       $all = $src + $zp
+       [System.IO.Compression.ZipFile]::ExtractToDirectory($all, $dest)
+   }
+}
 
- ZipEverything -src "\\sourcelocation\tracefiles\" -dest "\\destinationlocation\tracefiles\"
+ZipEverything -src "\\sourcelocation\tracefiles\" -dest "\\destinationlocation\tracefiles\"
+ 
+<#
+
+Function ZipEverything($src, $dest)
+{
+   [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+   $zps = Get-ChildItem $src -Filter *.zip
+   ## Move exceptions:
+   $exc = $dest + "Exceptions\"
+
+   foreach ($zp IN $zps)
+   {
+       $all = $src + $zp
+       try
+       {
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($all, $dest)
+            
+       }
+       catch
+       {
+            Move-Item $all $exc
+       }
+   }
+}
+
+ZipEverything -src "\\sourcelocation\tracefiles\" -dest "\\destinationlocation\tracefiles\"
+
+
+#>
