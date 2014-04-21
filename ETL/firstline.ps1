f887fd3795acf863d193238e3d16be86a41cd05f
Tim <tmmtsmith@gmail.com>
2014-04-21 13:27:05 -0500

    Update firstline.ps1

--- ETL/firstline.ps1
+++ ETL/firstline.ps1
@@ -2,6 +2,6 @@ $tName = $filename.Split("\") | Select-Object -Last 1
 $tName = $tName.Replace("$extension","")
 
 $f = Get-Content $filename | Select -First 1
-## SECOND HERE
+$f = $f.Replace("|", "] VARCHAR(MAX), [")
 $f = "IF OBJECT_ID('" + $tName + "') IS NOT NULL BEGIN DROP TABLE " + $tName + " END CREATE TABLE ["+ $tName +"] ([" + $f + "] VARCHAR(MAX))"
 ## Grabs the first line of the file
