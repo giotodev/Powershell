5727d940e343d1ccd7b88b2eb08e7a2937ff7222
Tim <tmmtsmith@gmail.com>
2014-05-02 10:25:25 -0500

    Source control replace

--- sourcecontrolreplace.ps1
+++ sourcecontrolreplace.ps1
@@ -1,1 +1,29 @@
-$x | out-file -FilePath "C:\Files\SQLFile.sql" -Force
+
+
+$s = ""
+$d = ""
+
+$scon = New-Object System.Data.SqlClient.SqlConnection
+$scon.ConnectionString = "SERVER=" + $s + ";DATABASE=" + $d + ";Integrated Security=true"
+$trycmd = New-Object System.Data.SqlClient.SqlCommand
+$catchcmd = New-Object System.Data.SqlClient.SqlCommand
+$trycmd.CommandText = ""
+$trycmd.Connection = $scon
+$catchcmd.CommandText = ""
+$catchcmd.Connection = $scon
+
+
+try
+{
+  $scon.Open()
+  $trycmd.ExecuteNonQuery()
+}
+catch
+{
+  $catchcmd.ExecuteNonQuery()
+}
+finally
+{
+  $scon.Close()
+  $scon.Dispose()
+}
