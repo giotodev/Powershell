81e9b8e48461d395f1d230ba7010e2865699f5b5
Tim <tmmtsmith@gmail.com>
2014-05-05 06:31:58 -0500

    Print all four screens (huge file)

---
+++ printfullscreen_allscreens.ps1
@@ -0,0 +1,11 @@
+﻿Function PrintScreen-Full ($loc) {
+    $vs = [Windows.Forms.SystemInformation]::VirtualScreen
+    $draw = New-Object Drawing.Bitmap $vs.Width, $vs.Height
+    $graph = [Drawing.Graphics]::FromImage($draw)
+    $graph.CopyFromScreen( $vs.Location, [Drawing.Point]::Empty, $vs.Size)
+    $graph.Dispose()
+    $draw.Save($loc)
+    $draw.Dispose()
+}
+
+PrintScreen-Full -loc "C:\files\full.jpg"
