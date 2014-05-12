1c9f0fd9fb9cfafd3cdf721f680500e7a12833f1
Tim <tmmtsmith@gmail.com>
2014-05-12 08:01:45 -0500

    Cleaned and corrected

--- Administration/UnzipMultipleZipFiles.ps1
+++ Administration/UnzipMultipleZipFiles.ps1
@@ -1,7 +1,5 @@
 Function UnzipEverything($src, $dest)
 {
-   ## Deprecated:
-   ##[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
    Add-Type -AssemblyName System.IO.Compression.FileSystem
    $zps = Get-ChildItem $src -Filter *.zip
 
@@ -10,14 +8,9 @@ Function UnzipEverything($src, $dest)
        $all = $src + $zp
        [System.IO.Compression.ZipFile]::ExtractToDirectory($all, $dest)
    }
-
-   ## Server, Database, Table - IMP ONLY
-   ##$scon = New-Object System.Data.SqlClient.SqlConnection
-   ##$scon.ConnectionString = "SERVER=" + $server + ";DATABASE=" + $database + ";Integrated Security=true"
 }
 
-ZipEverything -src "\\sourcelocation\tracefiles\" -dest "\\destinationlocation\tracefiles\extract\"
-
+UnzipEverything -src "\\sourcelocation\tracefiles\" -dest "\\destinationlocation\tracefiles\extract\"
 
 Function ZipEverything($src, $dest)
 {
@@ -25,4 +18,4 @@ Function ZipEverything($src, $dest)
    [System.IO.Compression.ZipFile]::CreateFromDirectory($src, $dest)
 }
 
-UnzipEverything -src "C\\destinationlocation\tracefiles\extract" -dest "\\sourcelocation\tracefiles\file.zip"
+ZipEverything -src "C\\destinationlocation\tracefiles\extract" -dest "\\sourcelocation\tracefiles\file.zip"
