d48fe2651260c7abfbe179046f1e54aa048a4c18
Tim <tmmtsmith@gmail.com>
2014-08-21 07:41:00 -0500

    Update d.ps1

--- DL/d.ps1
+++ DL/d.ps1
@@ -1,7 +1,25 @@
-$time = Get-Date
-$b_json = '{ "_id":"' + $time.ToString() + '", '
-$e_json = '}'
+Function DNEC_Rating($t, $c, $no = $null, $de = $null)
+{
+    $time = Get-Date
 
-## 4
+    $b_json = '{ _id: "' + $time.ToString() + '", '
+    $b_json += '"title": "' + $t + '"'
+
+    if ($no -ne "")
+    {
+        $b_json += ', "number": ' + $no
+    }
+
+    if ($de -ne "")
+    {
+        $b_json += ', "details": "' + $de + '"'
+    }
+
+    $b_json += ', "comments": "' + $c + '" }'
+    $b_json
+}
+
+DNEC_Rating -t "DEV" -c "ta7" -no 7 -de "Nw 7"
+
 ## JSON doc
 ## Auto bk
