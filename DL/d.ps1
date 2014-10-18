841f4959bfcc915a2749fcc2881bb77ea3bedbcf
Tim <tmmtsmith@gmail.com>
2014-10-18 04:59:53 -0500

    Update d.ps1

--- DL/d.ps1
+++ DL/d.ps1
@@ -1,24 +1,4 @@
 Function DNEC_Assessment($t, $c, $no = $null, $de = $null)
 {
-    $time = Get-Date
-
-    $b_json = '{ _id: "' + $time.ToString() + '", '
-    $b_json += '"title": "' + $t + '"'
-
-    if ($no -ne "")
-    {
-        $b_json += ', "number": ' + $no
-    }
-
-    if ($de -ne "")
-    {
-        $b_json += ', "details": "' + $de + '"'
-    }
-
-    $b_json += ', "comments": "' + $c + '" }'
-    $b_json
 }
-
-## EXAMPLE:
-DNEC_Assessment -t "DEV" -c "ta7" -no 7 -de "nw 7"
 
