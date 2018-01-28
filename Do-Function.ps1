fb3d019553537b6dfd91ddf0db27894294eca1a4
Tim <tmmtsmith@gmail.com>
2018-01-28 11:35:27 -0600

    Update Do-Function.ps1

--- Do-Function.ps1
+++ Do-Function.ps1
@@ -3,9 +3,11 @@ Function Do-Function {
   Param(
     [Parameter(Mandatory=$false)][string]$notrequired
     , [Parameter(Mandatory=$true)][ValidateSet("1","2","3","Other")]$set
+    , [Parameter(Mandatory=$true)][ValidateRange(0,10)]$range
   )
   Process
   {
     Write-Host "Is the parameter included?  $notrequired$set"
+    Write-Host (20/$range).ToString()
   }
 }
